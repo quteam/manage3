a939b516ac2519a607a2fe454207900a30449b6b
Haovei <haovei@qq.com>
2016-01-22 11:21:00 +0800

    自动完成

--- src/ui/modules/main/directives.js
+++ src/ui/modules/main/directives.js
@@ -867,6 +867,9 @@ define('main/directives', ['main/init'], function () {
     };
     relativeSelect.$inject = ["$timeout"];
 
+    /**
+     * 图表
+     */
     function eChart($http) {
         return {
             restrict: 'A',
@@ -906,6 +909,223 @@ define('main/directives', ['main/init'], function () {
     eChart.$inject = ["$http"];
 
     /**
+     * 自动补全
+     */
+    function angucomplete($parse, $http, $sce, $timeout) {
+        return {
+            restrict: 'EA',
+            scope: {
+                "placeholder": "@placeholder",
+                "selectedItem": "=selectedItem",
+                "url": "@url",
+                "titleField": "@titleField",
+                "descriptionField": "@descriptionField",
+                //"localData": "=localdata",
+                "searchFields": "@searchfields",
+                "matchClass": "@matchclass"
+            },
+            templateUrl: 'tpl/autocomplete.html',
+            link: function ($scope, elem, attrs) {
+                $scope.lastSearchTerm = null;
+                $scope.currentIndex = null;
+                $scope.justChanged = false;
+                $scope.searchTimer = null;
+                $scope.hideTimer = null;
+                $scope.searching = false;
+                $scope.pause = 300;
+                $scope.minLength = 1;
+                $scope.searchStr = null;
+
+                var isNewSearchNeeded = function (newTerm, oldTerm) {
+                    return newTerm.length >= $scope.minLength && newTerm != oldTerm
+                };
+
+                $scope.processResults = function (responseData, str) {
+                    if (responseData && responseData.length > 0) {
+                        $scope.results = [];
+
+                        var titleFields = [];
+                        if ($scope.titleField && $scope.titleField != "") {
+                            titleFields = $scope.titleField.split(",");
+                        }
+
+                        for (var i = 0; i < responseData.length; i++) {
+                            // Get title variables
+                            var titleCode = [];
+
+                            for (var t = 0; t < titleFields.length; t++) {
+                                titleCode.push(responseData[i][titleFields[t]]);
+                            }
+
+                            var description = "";
+                            if ($scope.descriptionField) {
+                                description = responseData[i][$scope.descriptionField];
+                            }
+
+                            var text = titleCode.join(' ');
+                            if ($scope.matchClass) {
+                                var re = new RegExp(str, 'i');
+                                var strPart = text.match(re)[0];
+                                text = $sce.trustAsHtml(text.replace(re, '<span class="' + $scope.matchClass + '">' + strPart + '</span>'));
+                            }
+
+                            var resultRow = {
+                                title: text,
+                                description: description,
+                                //image: image,
+                                originalObject: responseData[i]
+                            };
+
+                            $scope.results[$scope.results.length] = resultRow;
+                        }
+
+
+                    } else {
+                        $scope.results = [];
+                    }
+                };
+
+                $scope.searchTimerComplete = function (str) {
+                    // Begin the search
+
+                    if (str.length >= $scope.minLength) {
+                        if ($scope.localData) {
+                            var searchFields = $scope.searchFields.split(",");
+
+                            var matches = [];
+
+                            for (var i = 0; i < $scope.localData.length; i++) {
+                                var match = false;
+
+                                for (var s = 0; s < searchFields.length; s++) {
+                                    match = match || (typeof $scope.localData[i][searchFields[s]] === 'string' && typeof str === 'string' && $scope.localData[i][searchFields[s]].toLowerCase().indexOf(str.toLowerCase()) >= 0);
+                                }
+
+                                if (match) {
+                                    matches[matches.length] = $scope.localData[i];
+                                }
+                            }
+
+                            $scope.searching = false;
+                            $scope.processResults(matches, str);
+
+                        } else {
+                            $http.get($scope.url + str, {}).success(function (responseData, status, headers, config) {
+                                $scope.searching = false;
+                                if (responseData.code == 200) {
+                                    $scope.processResults(responseData.data, str);
+                                }
+                            }).error(function (data, status, headers, config) {
+                                console.log("error");
+                            });
+                        }
+                    }
+                };
+
+                $scope.hideResults = function () {
+                    $scope.hideTimer = $timeout(function () {
+                        $scope.showDropdown = false;
+                    }, $scope.pause);
+                };
+
+                $scope.resetHideResults = function () {
+                    if ($scope.hideTimer) {
+                        $timeout.cancel($scope.hideTimer);
+                    }
+                };
+
+                $scope.hoverRow = function (index) {
+                    $scope.currentIndex = index;
+                };
+
+                $scope.keyPressed = function (event) {
+                    if (!(event.which == 38 || event.which == 40 || event.which == 13)) {
+                        if (!$scope.searchStr || $scope.searchStr == "") {
+                            $scope.showDropdown = false;
+                            $scope.lastSearchTerm = null
+                        } else if (isNewSearchNeeded($scope.searchStr, $scope.lastSearchTerm)) {
+                            $scope.lastSearchTerm = $scope.searchStr;
+                            $scope.showDropdown = true;
+                            $scope.currentIndex = -1;
+                            $scope.results = [];
+
+                            if ($scope.searchTimer) {
+                                $timeout.cancel($scope.searchTimer);
+                            }
+
+                            $scope.searching = true;
+
+                            $scope.searchTimer = $timeout(function () {
+                                $scope.searchTimerComplete($scope.searchStr);
+                            }, $scope.pause);
+                        }
+                    } else {
+                        event.preventDefault();
+                    }
+                };
+
+                $scope.selectResult = function (result) {
+                    if ($scope.matchClass) {
+                        result.title = result.title.toString().replace(/(<([^>]+)>)/ig, '');
+                    }
+                    $scope.searchStr = $scope.lastSearchTerm = result.title;
+                    $scope.selectedItem = result;
+                    $scope.showDropdown = false;
+                    $scope.results = [];
+                    //$scope.$apply();
+                };
+
+                var inputField = elem.find('input');
+
+                inputField.on('keyup', $scope.keyPressed);
+
+                elem.on("keyup", function (event) {
+                    if (event.which === 40) {
+                        if ($scope.results && ($scope.currentIndex + 1) < $scope.results.length) {
+                            $scope.currentIndex++;
+                            $scope.$apply();
+                            event.preventDefault;
+                            event.stopPropagation();
+                        }
+
+                        $scope.$apply();
+                    } else if (event.which == 38) {
+                        if ($scope.currentIndex >= 1) {
+                            $scope.currentIndex--;
+                            $scope.$apply();
+                            event.preventDefault;
+                            event.stopPropagation();
+                        }
+
+                    } else if (event.which == 13) {
+                        if ($scope.results && $scope.currentIndex >= 0 && $scope.currentIndex < $scope.results.length) {
+                            $scope.selectResult($scope.results[$scope.currentIndex]);
+                            $scope.$apply();
+                            event.preventDefault;
+                            event.stopPropagation();
+                        } else {
+                            $scope.results = [];
+                            $scope.$apply();
+                            event.preventDefault;
+                            event.stopPropagation();
+                        }
+
+                    } else if (event.which == 27) {
+                        $scope.results = [];
+                        $scope.showDropdown = false;
+                        $scope.$apply();
+                    } else if (event.which == 8) {
+                        $scope.selectedItem = null;
+                        $scope.$apply();
+                    }
+                });
+
+            }
+        };
+    };
+    angucomplete.$inject = ["$parse", "$http", "$sce", "$timeout"];
+
+    /**
      * 加入项目
      */
     angular.module('manageApp.main')
@@ -924,4 +1144,5 @@ define('main/directives', ['main/init'], function () {
         .directive("selectAsync", selectAsync)
         .directive("relativeSelect", relativeSelect)
         .directive("chart", eChart)
+        .directive("angucomplete", angucomplete)
 });
