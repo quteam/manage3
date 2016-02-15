912eae3affee14def8fa3d33c528a479673ebe6e
Haovei <haovei@qq.com>
2016-02-15 19:41:07 +0800

    .

---
+++ src/ui/modules/project/init.js
@@ -0,0 +1,6 @@
+/**
+ * Created by hao on 15/11/11.
+ */
+define('project/init', ['angular'], function () {
+    angular.module('manageApp.project', []);
+});
---
+++ src/ui/modules/project/services.js
@@ -0,0 +1,6 @@
+/**
+ * Created by hao on 15/11/18.
+ */
+define('project/services', ['project/init'], function () {
+
+});
