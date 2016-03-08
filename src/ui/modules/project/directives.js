03be30e8a5458f88a53af6e9a9d784b6f79aa9c3
Haovei <haovei@qq.com>
2016-03-09 00:52:55 +0800

    .

--- src/ui/modules/project/directives.js
+++ src/ui/modules/project/directives.js
@@ -3,5 +3,46 @@
  */
 
 define('project/directives', ['project/init'], function () {
+    /**
+     * 自定义配置 (资源相关)
+     */
+    function customConfig($timeout) {
+        return {
+            restrict: 'AE',
+            scope: true,
+            transclude: true,
+            require: "?^ngModel",
+            link: function ($scope, $element, $attrs, ngModel, $transclude) {
+                $scope.dataList = [];
+
+                //$scope.$watch("dataList", function () {
+                //    var formData = {};
+                //    angular.forEach($scope.dataList, function (_row, _index) {
+                //        var _data = {};
+                //        for (var i = 0, l = _row.length; i < l; i++) {
+                //            _data[inputName[i]] = _row[i];
+                //        }
+                //        formData[_index] = _data;
+                //    });
+                //    ngModel.$setViewValue(formData);
+                //}, true);
+
+                $scope.addRow = function () {
+                    $scope.dataList.push({});
+                };
+
+                $scope.delRow = function (n) {
+                    $scope.dataList.splice(n, 1);
+                };
+
+                $transclude($scope, function (clone) {
+                    $element.append(clone);
+                });
+            }
+        };
+    };
+    customConfig.$inject = ["$timeout"];
+
     angular.module('manageApp.project')
+        .directive('customConfig', customConfig)
 });
--- src/ui/modules/project/services.js
+++ src/ui/modules/project/services.js
@@ -2,6 +2,5 @@
  * Created by hao on 15/11/18.
  */
 define('project/services', ['project/init'], function () {
-
     angular.module('manageApp.project')
 });
