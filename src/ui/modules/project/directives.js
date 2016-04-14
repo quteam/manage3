691435166e50a74fb5406dc2c87c893b577a18cc
Haovei <haovei@qq.com>
2016-04-14 23:09:21 +0800

    打分

--- src/ui/modules/project/directives.js
+++ src/ui/modules/project/directives.js
@@ -24,6 +24,39 @@ define('project/directives', ['moment', 'project/init'], function (moment) {
     };
     tableCalendar.$inject = ["requestData"];
 
+    /**
+     * 打分
+     */
+    function scoreConfig() {
+        return {
+            restrict: 'AE',
+            scope: {
+                teacherList: "=",
+                selectCount: "=?"
+            },
+            require: "?^ngModel",
+            transclude: true,
+            link: function ($scope, $element, $attrs, ngModel, $transclude) {
+                $scope.$watch("teacherList", function (value) {
+                    if (value) {
+                        var _count = 0;
+                        angular.forEach(value, function (v, k) {
+                            if (v.ruleIdList.length) {
+                                _count++;
+                            }
+                        });
+                        ngModel.$setViewValue(_count);
+                    }
+                }, true);
+
+                $transclude($scope, function (clone) {
+                    $element.append(clone);
+                });
+            }
+        };
+    };
+
     angular.module('manageApp.project')
         .directive('tableCalendar', tableCalendar)
+        .directive('scoreConfig', scoreConfig)
 });
